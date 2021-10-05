4a0d5075608458e129729064da0780381157f844
Ismail Ahmedov <iahmedov@paradigmadigital.com>
2021-10-06 00:26:22 +0200

    Refactor

--- src/test/java/StringCalculatorShould.java
+++ src/test/java/StringCalculatorShould.java
@@ -4,10 +4,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringCalculatorShould {
 
+    private final StringCalculator calculator = new StringCalculator();
+
     @Test
     void return_0_for_empty_string() {
-        StringCalculator calculator = new StringCalculator();
-
         int actual = calculator.add("");
 
         assertThat(actual).isEqualTo(0);
@@ -15,8 +15,6 @@ public class StringCalculatorShould {
 
     @Test
     void return_same_number_if_string_contains_single_number() {
-        StringCalculator calculator = new StringCalculator();
-
         int actual = calculator.add("1");
 
         assertThat(actual).isEqualTo(1);
