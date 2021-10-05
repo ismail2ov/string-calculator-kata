08dedb4770910fc4e388e41e6edc0303193c73bc
Ismail Ahmedov <iahmedov@paradigmadigital.com>
2021-10-06 01:24:15 +0200

    Refactor

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,6 +1,9 @@
 import java.util.Arrays;
 
 public class StringCalculator {
+
+    public static final String DELIMITER_PREFIX = "//";
+
     public int add(String input) {
         if (input.isEmpty()) {
             return 0;
@@ -9,12 +12,21 @@ public class StringCalculator {
         String delimiter = "[\n,]";
 
         int newLineIndex = input.indexOf("\n");
-        if ((newLineIndex > 1) && (input.startsWith("//"))) {
-            delimiter = input.substring(2, newLineIndex);
-            input = input.substring(newLineIndex + 1);
+        if ((newLineIndex > DELIMITER_PREFIX.length()) && (input.startsWith(DELIMITER_PREFIX))) {
+            return add(input, newLineIndex);
         }
+        return add(input, delimiter);
+    }
+
+    private int add(String input, String delimiter) {
         String[] arr = input.split(delimiter);
 
         return Arrays.stream(arr).mapToInt(number -> Integer.parseInt(number.trim())).sum();
+    }
+
+    private int add(String inputWithDelimiter, int newLineIndex) {
+        String delimiter = inputWithDelimiter.substring(DELIMITER_PREFIX.length(), newLineIndex);
+        String input = inputWithDelimiter.substring(newLineIndex + 1);
+        return add(input, delimiter);
     }
 }
